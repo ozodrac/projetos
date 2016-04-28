4bfca6c02cb01c4fca26d404c691ab1959899273
Deivid Cardoso <deividgama.net@hotmail.com>
2016-04-27 23:08:11 -0300

    Validação de formulario
    Macho, adorei esse sweet alert

--- CLINICA/js/scripts.js
+++ CLINICA/js/scripts.js
@@ -1,15 +1,34 @@
 function validar() {
-  var nome = formulario.nome.value;
-  var email = formulario.email.value;
-  var msg = formulario.msg.value;
+  var nome = document.getElementById('nome').value;
+  var email = document.getElementById('email').value;
+  var msg = document.getElementById('msg').value;
 
-if (nome,email,msg == "") {
-
-  alert("Preencha os campos em branco")
-    // formulario.nome.focus();
+  function IsEmail(email){
+  	var exclude=/[^@\-\.\w]|^[_@\.\-]|[\._\-]{2}|[@\.]{2}|(@)[^@]*\1/;
+  	var check=/@[\w\-]+\./;
+  	var checkend=/\.[a-zA-Z]{2,3}$/;
+  	if(((email.search(exclude) != -1)||(email.search(check)) == -1)||(email.search(checkend) == -1)){return false;}
+  	else {return true;}
+  }
+  if(email == ''){
+    document.getElementById('email').focus();
+    sweetAlert("Email vazio!", "Favor preencha o campo de email.", "error");
     return false;
- }
- else {
-   alert("Enviado com sucesso!")
- }
+  }
+  if (!IsEmail(email)){
+   document.getElementById('email').focus();
+   sweetAlert("Email inválido!", "Favor nos informe um email válido.", "error");
+   return false;
+  }
+  if (nome == "") {
+    document.getElementById('nome').focus();
+    sweetAlert("Nome Vazio!", "Favor preencha o campo nome.", "error");
+    return false;
+   }
+  if(msg == ''){
+    document.getElementById('msg').focus();
+    sweetAlert("Mensagem Vazia", "Vamos lá, nos envie uma mensagem.", "error");
+    return false;
+  }
+  swal("Good job!", "You clicked the button!", "success");
 }
